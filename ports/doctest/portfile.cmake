c860f26734e8fa0f399a32985b54bd161c2248ed
Oliver Dixon <od641@york.ac.uk>
2026-08-01 06:22:30 +0100

    Use oliverdixon/doctest fork for doctest.

--- ports/doctest/portfile.cmake
+++ ports/doctest/portfile.cmake
@@ -2,10 +2,10 @@ set(VCPKG_BUILD_TYPE release) # header-only library
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO doctest/doctest
-    REF "v${VERSION}"
-    SHA512 d7222ae62cf2ac0f858567af0e539bd34cef47827004f805261007943e8e02370fcf16cbf0302581766353bf28317dd280a885fdc775baaa3e519aa2cf2f2eca
-    HEAD_REF master
+    REPO oliverdixon/doctest
+    REF 89085a3a1ad49b6f8a8238de960104935b5377d6
+    SHA512 078afa8b98e9b8b815c76fd0ec397594aaa60a7893d0421f4ddbda0642371c56b88cbfdf552ec150e49d7b90ac8cda39e9260bbbcc485d02467797d5233b621e
+    HEAD_REF disable-colours-when-parsing
 )
 
 vcpkg_cmake_configure(
